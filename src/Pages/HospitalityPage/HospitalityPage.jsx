600736b4b3d920f5b430c282d221aa36d6def27e
Varun kumar <varunkumar953685@gmail.com>
2024-11-22 16:51:34 +0530

    16

--- src/Pages/HospitalityPage/HospitalityPage.jsx
+++ src/Pages/HospitalityPage/HospitalityPage.jsx
@@ -19,9 +19,9 @@ import vimg from "../../assets/insidePages/vimg.png"
 
 
 
-import c1 from "../../assets/Hospitality/customer/c1.png"
+import c3 from "../../assets/Hospitality/customer/c1.png"
 import c2 from "../../assets/Hospitality/customer/c2.png"
-import c3 from "../../assets/Hospitality/customer/c3.png"
+import c1 from "../../assets/Hospitality/customer/c3.png"
 
 
 import s1 from "../../assets/Hospitality/story/s1.png"
@@ -126,27 +126,27 @@ export default function HospitalityPage() {
     const clientLogo = [
 
         {
-            logo : tata
+            logo: tata
         },
 
         {
-            logo : pg
+            logo: pg
         },
 
         {
-            logo : slb
+            logo: slb
         },
 
         {
-            logo : salesvu
+            logo: salesvu
         },
 
         {
-            logo : indOil
+            logo: indOil
         },
 
         {
-            logo : nrf
+            logo: nrf
         }
     ]
 
@@ -154,23 +154,29 @@ export default function HospitalityPage() {
     const feedback = [
         {
             img: c1,
-            position: " Operations Manager, LuxeStay Hotels",
+            position: " Operations Manager",
+            company: ", LuxeStay Hotels",
             heading: " Transformative Technology Partner ",
-            para: " EICE Rise has completely transformed our operations. Their cloud ERP seamlessly integrates all aspects of our hotel, from reservations to dining, improving efficiency and enhancing our guest experience. We couldn’t ask for a better technology partner."
+            para: " EICE Rise has completely transformed our operations. Their cloud ERP seamlessly integrates all aspects of our hotel, from reservations to dining, improving efficiency and enhancing our guest experience. We couldn’t ask for a better technology partner.",
+
         },
 
         {
             img: c2,
-            position: " Operations Manager, LuxeStay Hotels",
-            heading: " Transformative Technology Partner ",
-            para: " EICE Rise has completely transformed our operations. Their cloud ERP seamlessly integrates all aspects of our hotel, from reservations to dining, improving efficiency and enhancing our guest experience. We couldn’t ask for a better technology partner."
+            position: "General Manager",
+            company: ", Serenity Resorts",
+            heading: " Exceptional Support and Innovation ",
+            para: " The intuitive modules offered by EICE Rise have simplified our front desk and housekeeping operations. Their team’s support and innovative approach are unmatched. We’ve seen remarkable growth since implementing their solutions.",
+
         },
 
         {
             img: c3,
-            position: " Operations Manager, LuxeStay Hotels",
-            heading: " Transformative Technology Partner ",
-            para: " EICE Rise has completely transformed our operations. Their cloud ERP seamlessly integrates all aspects of our hotel, from reservations to dining, improving efficiency and enhancing our guest experience. We couldn’t ask for a better technology partner."
+            position: " Director of Operations",
+            company: ", Gourmet Suites",
+            heading: "Elevated, Efficiency Guaranteed",
+            para: "Thanks to EICE Rise, we’ve streamlined processes like banquet management and inventory control. The centralized platform has saved us time and significantly improved guest satisfaction.",
+
         }
     ]
 
@@ -179,19 +185,22 @@ export default function HospitalityPage() {
         {
             img: s1,
             heading: " Seamless Operations at LuxeStay Hotels",
+            margin: "20px",
             para: "How EICE Rise streamlined front desk, housekeeping, and POS systems, boosting operational efficiency by 40%. "
         },
 
         {
             img: s2,
             heading: "Driving Guest Satisfaction for Serenity Resorts",
-            para: "Enhanced guest experiences through AI-driven personalization and integrated feedback systems. "
+            para: "Enhanced guest experiences through AI-driven personalization and integrated feedback systems. ",
+            margin: "20px"
         },
 
         {
             img: s3,
             heading: " Expanding Global Reach with Gourmet Suites ",
-            para: " Scaling operations across multiple properties with centralized data and real-time insights. "
+            para: " Scaling operations across multiple properties with centralized data and real-time insights. ",
+            margin: "50px"
         }
 
     ]
@@ -265,7 +274,7 @@ export default function HospitalityPage() {
             <section className={`${styles.section2} `}>
 
 
-                <div className={`${styles.section2Heading} font1 globalPageSize`}>Our <span className={`${styles.headingClass} font2`} > End to End </span>  Technology Solution</div>
+                <div className={`${styles.section2Heading} font1 globalPageSize`}>Our <span className={` blueTextGlobalClass font2`} > End to End </span>  Technology Solution</div>
 
 
 
@@ -300,7 +309,7 @@ export default function HospitalityPage() {
 
             <section className={`${styles.section3} globalSectionSize`}>
 
-                <div className={`${styles.section3Heading} font1`}>With deep expertise in <span className={`${styles.headingClass} font2`}>Hospitality Technology</span> , we have
+                <div className={`${styles.section3Heading} font1`}>With deep expertise in <span className={` blueTextGlobalClass font2`}>Hospitality Technology</span> , we have
                     perfected the art of delivering efficient, scalable, and tailored
                     tech solutions for the industry</div>
 
@@ -324,7 +333,7 @@ export default function HospitalityPage() {
             <section className={`${styles.section4} globalSectionSize`}>
 
                 <div className={`${styles.section4Heading}`}>
-                    <span className={`${styles.headingClass} font2`}>Trusted</span><span className="font1"> by Brands around the World</span>
+                    <span className={` blueTextGlobalClass font2`}>Trusted</span><span className="font1"> by Brands around the World</span>
                 </div>
             </section>
 
@@ -332,10 +341,10 @@ export default function HospitalityPage() {
             <div className={styles.clientSection} >
                 <div className={`${styles.clientLogoBox} globalSectionSize`}>
                     {/* <img style={{ width: "100%" }} src={client} alt="" /> */}
-                    {clientLogo.map((item)=>
+                    {clientLogo.map((item) =>
                     (
-                        <div style={{width : "12%"}}>
-                            <img  style={{width : "100%"}} src={item.logo} alt="" />
+                        <div style={{ width: "12%" }}>
+                            <img style={{ width: "100%" }} src={item.logo} alt="" />
                         </div>
 
                     ))}
@@ -355,12 +364,12 @@ export default function HospitalityPage() {
             <section className={`${styles.section5} globalSectionSize`}>
 
                 <div className={`${styles.section5Heading} font1`}>
-                    What our <span className={`${styles.headingClass} font2`}>Customers</span> say about us ?
+                    What our <span className={` blueTextGlobalClass font2`}>Customers</span> say about us ?
                 </div>
 
 
                 <div className={`${styles.section5Subheading} font1`}>
-                    "<span style={{ fontWeight: "600" }} > Proven Excellence:</span> Trusted by Leading  <span className={`${styles.headingClass} font2`}>Hotels, Resorts, and Restaurants </span>."
+                    "<span style={{ fontWeight: "600" }} > Proven Excellence:</span> Trusted by Leading  <span className={` blueTextGlobalClass font2`}>Hotels, Resorts, and Restaurants </span>."
                 </div>
 
 
@@ -376,13 +385,13 @@ export default function HospitalityPage() {
                                     <img style={{ width: "28%" }} src={item.img} alt="" />
                                 </div>
 
-                                <figcaption style={{ textAlign: "center" }} >{item.position}</figcaption>
+                                <figcaption style={{ textAlign: "center" }} > <span style={{ fontWeight: "bold" }}>{item.position}</span> <span>{item.company}</span></figcaption>
 
                             </figure>
 
-                            <div style={{ textAlign: "center" }}>{item.heading}</div>
+                            <div className={`${styles.cardBoxInnerHeading} blueTextGlobalClass font1`} style={{ textAlign: "center" }}>{item.heading}</div>
 
-                            <p style={{ textAlign: "center" }}>{item.para}</p>
+                            <p style={{ textAlign: "center", lineHeight: "1.8rem" }}>{item.para}</p>
 
                         </article>
                     ))}
@@ -400,11 +409,11 @@ export default function HospitalityPage() {
             {/* section6 */}
             <section className={`${styles.section6} globalSectionSize`}>
                 <div className={`${styles.section6Heading} font1`}>
-                    Real  <span className={`${styles.headingClass} font2`}>Stories</span> , Real <span className={`${styles.headingClass} font2`}>Impact</span>
+                    Real  <span className={` blueTextGlobalClass font2`}>Stories</span> , Real <span className={` blueTextGlobalClass font2`}>Impact</span>
                 </div>
 
 
-                <div className={`${styles.storySection}`}>
+                <div className={`${styles.storySection} font1`}>
 
                     {stories.map((item) =>
                     (
@@ -414,11 +423,15 @@ export default function HospitalityPage() {
                                 <img style={{ width: "100%" }} src={item.img} alt="storyimg" />
                             </div>
 
-                            <div>
+                            <div className={`${styles.cardBoxInnerHeading} blueTextGlobalClass font1`}>
                                 {item.heading}
                             </div>
 
-                            <p>{item.para}</p>
+                            <p style={{ lineHeight: "1.8rem", marginBottom: item.margin }} >{item.para}</p>
+
+                            <div className={`${styles.viewMoreBtn} font1`} style={{ textAlign: "center" }}>
+                                View More
+                            </div>
 
                         </div>
 
@@ -441,10 +454,10 @@ export default function HospitalityPage() {
 
 
                 <div className={`${styles.section7Heading} font1`}>
-                    Some Interesting <span className={`${styles.headingClass} font2`} >Insights</span> from the Hospitality Industry
+                    Some Interesting <span className={` blueTextGlobalClass font2`} >Insights</span> from the Hospitality Industry
                 </div>
 
-                <div className={`${styles.insightSection}`}>
+                <div className={`${styles.insightSection} font1`}>
 
                     {insights.map((item) => (
                         <div className={`${styles.insightBox}`}>
@@ -453,8 +466,8 @@ export default function HospitalityPage() {
                                 <img style={{ width: "100%" }} src={item.img} alt="" />
                             </div>
 
-                            <div>{item.heading}</div>
-                            <p>{item.para}</p>
+                            <div className={`${styles.cardBoxInnerHeading} blueTextGlobalClass font1`}>{item.heading}</div>
+                            <p style={{ lineHeight: "1.8rem" }}  >{item.para}</p>
                         </div>
                     ))}
                 </div>
