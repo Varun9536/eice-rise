6dbeddbc587fea48b9f3086b2c8bd163a7f1ec6e
Varun kumar <varunkumar953685@gmail.com>
2025-01-27 14:29:43 +0530

    change

--- src/Components/PhoneNavigationBar/topNavbar/TopNavBar.jsx
+++ src/Components/PhoneNavigationBar/topNavbar/TopNavBar.jsx
@@ -25,7 +25,7 @@ export default function TopNavBar({ sendDataToParent , change }) {
         <>
 
             <div>
-                {!navIcon ?
+                {navIcon ?
                     (< IoCloseSharp className={styles.menuIcon} onClick={() => { handleIconChange(), sendDataToParent() }} size={36} />) :
                     (<HiOutlineBars3 className={styles.closeIcon} onClick={() => { handleIconChange(), sendDataToParent() }} size={36} />)}
             </div>
