238e18284a47cbac849d666c5bfb0fccef23b701
Varun kumar <varunkumar953685@gmail.com>
2025-01-31 16:11:53 +0530

    final durgabari

--- src/Pages/CaseStudy/Durgabari/Durgabari.jsx
+++ src/Pages/CaseStudy/Durgabari/Durgabari.jsx
@@ -8,7 +8,9 @@ import { Link } from "react-router-dom"
 import account from "../../../assets/Hospitality/allPageIcon/Account.png"
 import pos from "../../../assets/Hospitality/allPageIcon/POS.png"
 import banquet from "../../../assets/Hospitality/allPageIcon/Banquet.png"
+
 import member from "../../../assets/Hospitality/allPageIcon/Member.png"
+
 import room from "../../../assets/Hospitality/allPageIcon/Room.png"
 
 
@@ -242,6 +244,8 @@ export default function Durgabari() {
 
             <section className={`${styles.section2} `}>
 
+
+
                 <div className={`${styles.moduleboxAndHeading}  globalSectionSize `} >
 
                     <div className={`${styles.moduleIncludedHeading} font4`}>The Enterprise Suite included the following modules:</div>
@@ -258,16 +262,24 @@ export default function Durgabari() {
                                     <div className={styles.imgBox} >
                                         <img src={item.icon} alt="" />
                                     </div>
-
                                     <div>
                                         {item.key < 9 ? (<div className={`${styles.serviceName} font1`}>{item.serviceName}</div>) : (<div className={`${styles.serviceName2} font1`}>{item.serviceName}</div>)}
                                     </div>
+
                                 </div>
+
                             </Link>
+
                         ))}
                     </div>
 
                 </div>
+
+
+
+
+
+
             </section>
 
 
@@ -278,6 +290,8 @@ export default function Durgabari() {
                 </div>
 
                 <div className={`${styles.informationBox} font1 `}>
+
+                    
 
                     <div className={`${styles.statement2}`} >
                         <ul>
@@ -291,7 +305,12 @@ export default function Durgabari() {
                             ))}
                         </ul>
 
+
+
                     </div>
+
+                    
+
                 </div>
 
             </section>
@@ -340,8 +359,23 @@ export default function Durgabari() {
                                 </div>
 
                             </div>
+
+
+
+
+
+
+
+
                         ))}
+
+
+
+
                     </div>
+
+                   
+
                 </div>
 
             </section>
@@ -364,6 +398,8 @@ export default function Durgabari() {
                         ))}
                     </ul>
 
+
+
                 </div>
 
             </section>
@@ -381,6 +417,7 @@ export default function Durgabari() {
 
 
                 <div className={`${styles.conclusionPara} font1`}>
+                    {/* By implementing the Enterprise Suite, <span className="font2 blueTextGlobalClass">EICE Technology empowered IIC to overcome its operational challenges, enhance security, and foster seamless multi-user interactions</span> . The result was a scalable, efficient, and secure system that aligned with IIC's vision for the future. */}
                     The implementation of the <span className="font2 blueTextGlobalClass" >Club Suite was a pivotal moment for the Houston Durga Bari Society</span> . By addressing their operational challenges holistically, <span className="font2 blueTextGlobalClass" >EICE Technology enabled HDBS to embrace a future-ready, scalable platform</span>  that not only optimized their workflows but also reinforced their cultural and religious values across their global community
                 </div>
 
