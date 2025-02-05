d6146199aab1cace9cafbd02447c4ecc4b59f440
Varun kumar <varunkumar953685@gmail.com>
2025-02-05 17:18:00 +0530

    code optimize

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteCompression({ algorithm: 'brotliCompress' }) // Enables Brotli compression
+  ],
+  build: {
+    target: 'esnext', // Use latest JavaScript features
+    minify: 'terser', // Minify JS for smaller size
+    sourcemap: false, // Disable source maps in production
+    rollupOptions: {
+      external: ['react', 'react-dom'], // Use CDN instead of bundling React
+    },
+  },
+});
